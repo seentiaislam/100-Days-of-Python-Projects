0cd18f0fbda9d0892933d4f47db07f860a26c216
Seentia Islam <90871547+seentiaislam@users.noreply.github.com>
2023-01-27 02:30:30 -0500

    Add files via upload

---
+++ band_name_generator/main.py
@@ -0,0 +1,7 @@
+#Go to: https://replit.com/@appbrewery/band-name-generator-start?v=1
+
+print("Welcome to the Band Name Generator!")
+street = input("What is the name of the city that you grew up in?\n")
+pet = input("What is your pet's name?\n")
+color = input("What is your favorite color?\n")
+print("Your band name could be " + street + " " + pet + " " + color)
